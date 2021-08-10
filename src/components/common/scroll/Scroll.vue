a600273af85b4681d129867b161245995b07c23a
Dissolvesprme <85940151+Dissolvesprme@users.noreply.github.com>
2021-08-10 16:14:20 +0800

    '初始化项目'

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+
+export default {
+  name: "Scroll",
+  props: {
+    probeType: {
+      type: Number,
+      default: 0
+    },
+    pullUpLoad: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      scroll: null,
+      message: "哈哈哈"
+    };
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad,
+      click: true,
+      mouseWheel: true,
+      observeDOM: true,
+      observeImage: true
+    });
+    this.scroll.on("scroll", position => {
+      // console.log(position);
+      this.$emit("scroll", position);
+    });
+    this.scroll.on("pullingUp", () => {
+      this.$emit("pullingUp");
+    });
+  },
+  methods: {
+    scrollTo(x, y, time = 300) {
+      this.scroll.scrollTo(x, y, time); // scrollTo(x, y, time, easing, extraTransform, isSilent)ref：
+      // 如果在普通的 DOM 元素上使用，引用指向的就是 DOM 元素；如果用在子组件上，引用就指向组件实例，可以通过实例直接调用组件的方法或访问数据
+    },
+    finishPullUp() {
+      this.scroll.finishPullUp();
+    }
+  }
+};
+</script>
+
+<style></style>
